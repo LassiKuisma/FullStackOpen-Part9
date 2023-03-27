01c9cf2bb51093b7efcd4196eb90aaac34802bc7
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-03-27 20:35:35 +0300

    example: type assertions & eslint

--- example-typescript/calculator.ts
+++ example-typescript/calculator.ts
@@ -1,8 +1,6 @@
-type Operation = 'multiply' | 'add' | 'divide';
+export type Operation = 'multiply' | 'add' | 'divide';
 
-type Result = string | number;
-
-const calculator = (a: number, b: number, op: Operation): number => {
+export const calculator = (a: number, b: number, op: Operation): number => {
   switch (op) {
     case 'multiply':
       return a * b;
@@ -15,14 +13,3 @@ const calculator = (a: number, b: number, op: Operation): number => {
       throw new Error('Operation is not multiply, add or divide!');
   }
 };
-
-try {
-  console.log('2+4 =', calculator(2, 4, 'add'));
-  console.log('7/0 =', calculator(7, 0, 'divide'));
-} catch (error) {
-  let errorMessage = 'Something went wrong: ';
-  if (error instanceof Error) {
-    errorMessage += error.message;
-  }
-  console.log(errorMessage);
-}
