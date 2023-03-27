91776ffce3636c13f90cee41249ee887cf5c73bd
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-03-27 16:01:09 +0300

    exercise 9.1

---
+++ exercises/bmiCalculator.ts
@@ -0,0 +1,25 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const meters = height / 100;
+  const bmi = weight / (meters * meters);
+
+  const bmiCategories = [
+    { name: 'Underweight (Severe thinness)', maxBmi: 16.0 },
+    { name: 'Underweight (Moderate thinness)', maxBmi: 16.9 },
+    { name: 'Underweight (Mild thinness)', maxBmi: 18.4 },
+    { name: 'Normal range', maxBmi: 24.9 },
+    { name: 'Overweight (Pre-obese)', maxBmi: 29.9 },
+    { name: 'Obese (Class I)', maxBmi: 34.9 },
+    { name: 'Obese (Class II)', maxBmi: 39.9 },
+  ];
+  const highest = { name: 'Obese (Class III)', maxBmi: 999.9 };
+
+  for (const category of bmiCategories) {
+    if (bmi < category.maxBmi) {
+      return category.name;
+    }
+  }
+
+  return highest.name;
+};
+
+console.log(calculateBmi(180, 74));
