ae2463351c7f247fd8b9a2a93c90920ed8df808b
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-04-03 14:43:14 +0300

    change port, add cors

--- example-flight-diaries/src/index.ts
+++ example-flight-diaries/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import diaryRouter from './routes/diaries';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 
 app.get('/ping', (_req, res) => {
   console.log('somebody pinged');
@@ -11,7 +14,7 @@ app.get('/ping', (_req, res) => {
 
 app.use('/api/diaries', diaryRouter);
 
-const PORT = 3000;
+const PORT = 3009;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
