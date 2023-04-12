8c98ba6cd052a6209fdf53177f18668c6e2d5473
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-04-12 16:43:14 +0300

    example: entries

--- patientor/server/src/types.ts
+++ patientor/server/src/types.ts
@@ -7,7 +7,7 @@ export interface Diagnosis {
 export enum Gender {
   Male = 'male',
   Female = 'female',
-  other = 'other',
+  Other = 'other',
 }
 
 export interface PatientPrivateInfo {
@@ -24,5 +24,48 @@ export type PatientInfo = Omit<PatientPrivateInfo, 'ssn' | 'entries'>;
 
 export type NewPatientEntry = Omit<PatientPrivateInfo, 'id'>;
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
