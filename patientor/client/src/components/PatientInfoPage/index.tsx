fdca2bd84fc12ecdd30331e1b87bf0ce18f74f5d
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-04-13 16:31:54 +0300

    wip 9.25: distinct look for entries

--- patientor/client/src/components/PatientInfoPage/index.tsx
+++ patientor/client/src/components/PatientInfoPage/index.tsx
@@ -2,9 +2,21 @@ import { useState, useEffect } from 'react';
 
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from '@mui/icons-material/Female';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import WorkIcon from '@mui/icons-material/Work';
+import HealingIcon from '@mui/icons-material/Healing';
 
-import { Diagnosis, Entry, Gender, Patient } from '../../types';
+import {
+  Diagnosis,
+  Entry,
+  Gender,
+  HealthCheckEntry,
+  HospitalEntry,
+  OccupationalHealthcareEntry,
+  Patient,
+} from '../../types';
 import patientService from '../../services/patients';
+import { assertNever } from '../../util';
 
 interface Props {
   id: string | undefined;
@@ -22,6 +34,48 @@ const genderIcon = (gender: Gender) => {
   }
 };
 
+const HospitalEntryDetails = ({ entry }: { entry: HospitalEntry }) => {
+  return (
+    <div className="entryBox">
+      {entry.date} <HealingIcon />
+      <br />
+      <i>{entry.description}</i>
+      <div>Hospital</div>
+      <br />
+      Diagnose by {entry.specialist}
+    </div>
+  );
+};
+
+const OccupationalEntryDetails = ({
+  entry,
+}: {
+  entry: OccupationalHealthcareEntry;
+}) => {
+  return (
+    <div className="entryBox">
+      {entry.date} <WorkIcon /> {entry.employerName}
+      <br />
+      <i>{entry.description}</i>
+      <br />
+      Diagnose by {entry.specialist}
+    </div>
+  );
+};
+
+const HealthCheckDetails = ({ entry }: { entry: HealthCheckEntry }) => {
+  return (
+    <div className="entryBox">
+      {entry.date} <MedicalServicesIcon />
+      <br />
+      <i>{entry.description}</i>
+      <div>Health rating: {entry.healthCheckRating.toString()}</div>
+      <br />
+      Diagnose by {entry.specialist}
+    </div>
+  );
+};
+
 const EntryDetails = ({
   entry,
   diagnoses,
@@ -29,7 +83,18 @@ const EntryDetails = ({
   entry: Entry;
   diagnoses: Map<string, Diagnosis>;
 }) => {
-  const codes = (
+  switch (entry.type) {
+    case 'Hospital':
+      return <HospitalEntryDetails entry={entry} />;
+    case 'OccupationalHealthcare':
+      return <OccupationalEntryDetails entry={entry} />;
+    case 'HealthCheck':
+      return <HealthCheckDetails entry={entry} />;
+    default:
+      return assertNever(entry);
+  }
+
+  /*const codes = (
     <ul>
       {entry.diagnosisCodes?.map((code) => {
         const name = diagnoses.get(code)?.name || 'unknown';
@@ -49,7 +114,7 @@ const EntryDetails = ({
       <i>{entry.description}</i>
       {codes}
     </div>
-  );
+  );*/
 };
 
 const Entries = ({
