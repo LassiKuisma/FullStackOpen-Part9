bf88473b5c4c3df7d4ecfdd55d8bf13eeeb03cf3
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-04-16 16:01:52 +0300

    service for adding entry

--- patientor/client/src/services/patients.ts
+++ patientor/client/src/services/patients.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Patient, PatientFormValues } from '../types';
+import { Entry, EntryWithoutId, Patient, PatientFormValues } from '../types';
 
 import { apiBaseUrl } from '../constants';
 
@@ -21,9 +21,19 @@ const getPatient = async (id: string) => {
   return data;
 };
 
+const addEntry = async (patient: Patient, entry: EntryWithoutId) => {
+  const { data } = await axios.post<Entry>(
+    `${apiBaseUrl}/patients/${patient.id}/entries`,
+    entry
+  );
+
+  return data;
+};
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   getAll,
   create,
   getPatient,
+  addEntry,
 };
