fe946d46205d88f0da6ba6fac4b82add3fe5d534
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-04-19 15:53:57 +0300

    support all types of entries

--- patientor/client/src/components/PatientInfoPage/NewEntryForm.tsx
+++ patientor/client/src/components/PatientInfoPage/NewEntryForm.tsx
@@ -1,7 +1,15 @@
-import { Box, Button, Grid, Tab, Tabs, TextField } from '@mui/material';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Grid,
+  Tab,
+  Tabs,
+  TextField,
+} from '@mui/material';
 import React, { SyntheticEvent, useState } from 'react';
-import { EntryWithoutId } from '../../types';
-import { stringToHealthCheckRating } from '../../util';
+import { Entry, EntryWithoutId } from '../../types';
+import { assertNever, stringToHealthCheckRating } from '../../util';
 
 interface Props {
   showWhenVisible: React.CSSProperties;
@@ -9,6 +17,15 @@ interface Props {
   submitNewEntry: (entry: EntryWithoutId) => Promise<boolean>;
   showError: (message: string) => void;
 }
+
+const buttonStyle: React.CSSProperties = {
+  marginTop: '5px',
+  marginBottom: '5px',
+};
+
+const inputStyle: React.CSSProperties = {
+  margin: '3px',
+};
 
 const NewEntryForm = ({
   showWhenVisible,
@@ -19,36 +36,200 @@ const NewEntryForm = ({
   const [description, setDescription] = useState('');
   const [date, setDate] = useState('');
   const [specialist, setSpecialist] = useState('');
-  const [healthRating, setHealthRating] = useState('');
   const [codes, setCodes] = useState('');
+
+  const [healthRating, setHealthRating] = useState('');
+
+  const [employerName, setEmployerName] = useState('');
+  const [sickLeaveEnabled, setSickLeaveEnabled] = useState(false);
+  const [sickLeaveStart, setSickLeaveStart] = useState('');
+  const [sickLeaveEnd, setSickLeaveEnd] = useState('');
+
+  const [dischargeCriteria, setDischargeCriteria] = useState('');
+  const [dischargeDate, setDischargeDate] = useState('');
+
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const healthCheck = (
+    <div>
+      <TextField
+        label="Healthcheck rating"
+        variant="standard"
+        fullWidth
+        value={healthRating}
+        onChange={({ target }) => setHealthRating(target.value)}
+        style={inputStyle}
+      />
+    </div>
+  );
+
+  const occupational = (
+    <div>
+      <TextField
+        label="Employer"
+        variant="standard"
+        fullWidth
+        value={employerName}
+        onChange={({ target }) => setEmployerName(target.value)}
+        style={inputStyle}
+      />
+      <div>
+        Sick leave?
+        <Checkbox
+          value={sickLeaveEnabled}
+          onChange={() => setSickLeaveEnabled(!sickLeaveEnabled)}
+        />
+      </div>
+      <TextField
+        label="Start"
+        variant="standard"
+        fullWidth
+        value={sickLeaveStart}
+        onChange={({ target }) => setSickLeaveStart(target.value)}
+        style={inputStyle}
+        disabled={!sickLeaveEnabled}
+      />
+      <TextField
+        label="End"
+        variant="standard"
+        fullWidth
+        value={sickLeaveEnd}
+        onChange={({ target }) => setSickLeaveEnd(target.value)}
+        style={inputStyle}
+        disabled={!sickLeaveEnabled}
+      />
+    </div>
+  );
+
+  const hospitalEntry = (
+    <div>
+      <TextField
+        label="Discharge criteria"
+        variant="standard"
+        fullWidth
+        value={dischargeCriteria}
+        onChange={({ target }) => setDischargeCriteria(target.value)}
+        style={inputStyle}
+      />
+      <TextField
+        label="Discharge date"
+        variant="standard"
+        fullWidth
+        value={dischargeDate}
+        onChange={({ target }) => setDischargeDate(target.value)}
+        style={inputStyle}
+      />
+    </div>
+  );
+
+  const tabs: Array<EntryTab> = [
+    {
+      entryType: 'HealthCheck',
+      label: 'Health check',
+      component: healthCheck,
+    },
+    {
+      entryType: 'OccupationalHealthcare',
+      label: 'Occupational',
+      component: occupational,
+    },
+    {
+      entryType: 'Hospital',
+      label: 'Hospital entry',
+      component: hospitalEntry,
+    },
+  ];
 
   const resetTextFields = () => {
     setDescription('');
     setDate('');
     setSpecialist('');
-    setHealthRating('');
     setCodes('');
+
+    setHealthRating('');
+
+    setEmployerName('');
+    setSickLeaveEnabled(false);
+    setSickLeaveStart('');
+    setSickLeaveEnd('');
+
+    setDischargeCriteria('');
+    setDischargeDate('');
   };
 
   const handleSubmit = async (event: SyntheticEvent) => {
     event.preventDefault();
 
-    const rating = stringToHealthCheckRating(healthRating);
-    if (rating.k === 'error') {
-      showError(`Invalid health check rating: ${rating.message}`);
-      return;
+    const codesSplit =
+      codes.length !== 0
+        ? codes.split(',').map((code) => code.trim())
+        : undefined;
+
+    const type = tabs[selectedTab].entryType;
+    let newEntry: EntryWithoutId | undefined = undefined;
+
+    switch (type) {
+      case 'HealthCheck':
+        const rating = stringToHealthCheckRating(healthRating);
+        if (rating.k === 'error') {
+          showError(`Invalid health check rating: ${rating.message}`);
+          return;
+        }
+
+        newEntry = {
+          type: 'HealthCheck',
+          description,
+          date,
+          specialist,
+          diagnosisCodes: codesSplit,
+          healthCheckRating: rating.value,
+        };
+        break;
+
+      case 'OccupationalHealthcare':
+        const sickLeave = sickLeaveEnabled
+          ? {
+              startDate: sickLeaveStart,
+              endDate: sickLeaveEnd,
+            }
+          : undefined;
+
+        newEntry = {
+          type: 'OccupationalHealthcare',
+          description,
+          date,
+          specialist,
+          diagnosisCodes: codesSplit,
+          employerName,
+          sickLeave,
+        };
+        break;
+
+      case 'Hospital':
+        const discharge = {
+          date: dischargeDate,
+          criteria: dischargeCriteria,
+        };
+
+        newEntry = {
+          type: 'Hospital',
+          description,
+          date,
+          specialist,
+          diagnosisCodes: codesSplit,
+          discharge,
+        };
+        break;
+
+      default:
+        assertNever(type);
+        break;
     }
 
-    const codesSplit = codes.split(',').map((code) => code.trim());
-
-    const newEntry: EntryWithoutId = {
-      type: 'HealthCheck',
-      description,
-      date,
-      specialist,
-      diagnosisCodes: codesSplit,
-      healthCheckRating: rating.value,
-    };
+    if (!newEntry) {
+      showError(`Entry type is invalid: '${type}'`);
+      return;
+    }
 
     const success = await submitNewEntry(newEntry);
     if (!success) {
@@ -63,15 +244,6 @@ const NewEntryForm = ({
   const onCancel = () => {
     closeEntryForm();
     resetTextFields();
-  };
-
-  const buttonStyle: React.CSSProperties = {
-    marginTop: '5px',
-    marginBottom: '5px',
-  };
-
-  const inputStyle: React.CSSProperties = {
-    margin: '3px',
   };
 
   return (
@@ -111,18 +283,9 @@ const NewEntryForm = ({
           style={inputStyle}
         />
         <EntryTabs
-          healthCheck={
-            <TextField
-              label="Healthcheck rating"
-              variant="standard"
-              fullWidth
-              value={healthRating}
-              onChange={({ target }) => setHealthRating(target.value)}
-              style={inputStyle}
-            />
-          }
-          occupational={<div>second tab</div>}
-          hospital={<div>third tab</div>}
+          tabs={tabs}
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
         />
         <Grid container justifyContent={'space-between'}>
           <Grid item>
@@ -152,41 +315,41 @@ const NewEntryForm = ({
   );
 };
 
-interface EntryTabsProps {
-  healthCheck: React.ReactNode;
-  occupational: React.ReactNode;
-  hospital: React.ReactNode;
+interface EntryTab {
+  component: React.ReactNode;
+  label: string;
+  entryType: Entry['type'];
 }
 
-const EntryTabs = ({ healthCheck, occupational, hospital }: EntryTabsProps) => {
-  const [value, setValue] = useState(0);
+interface EntryTabsProps {
+  tabs: Array<EntryTab>;
+  selectedTab: number;
+  setSelectedTab: (selected: number) => void;
+}
 
+const EntryTabs = ({ tabs, selectedTab, setSelectedTab }: EntryTabsProps) => {
   const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
+    setSelectedTab(newValue);
   };
 
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs
-          value={value}
+          value={selectedTab}
           onChange={handleChange}
           aria-label="entry type selection tabs"
         >
-          <Tab label="Health check" />
-          <Tab label="Occupational" />
-          <Tab label="Hospital entry" />
+          {tabs.map((tab) => (
+            <Tab label={tab.label} key={tab.label} />
+          ))}
         </Tabs>
       </Box>
-      <TabPanel value={value} index={0}>
-        {healthCheck}
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        {occupational}
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        {hospital}
-      </TabPanel>
+      {tabs.map((tab, index) => (
+        <TabPanel value={selectedTab} index={index} key={tab.label}>
+          {tab.component}
+        </TabPanel>
+      ))}
     </Box>
   );
 };
